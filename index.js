7e38261259df9917faa0c82c58a8ba8bf66e4e5e
mohammadsaifkhan <44085821+mohammadsaifkhan@users.noreply.github.com>
2019-01-04 09:32:54 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,63 @@
+'use strict';
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const http = require('https');
+// const API_KEY = require('./apiKey');
+// const request = require('request');
+
+const server = express();
+server.use(bodyParser.urlencoded({
+    extended: true
+}));
+
+server.use(bodyParser.json());
+
+server.post('/get-details', (req, res) => {
+
+    const dst = req.body.queryResult.parameters.destination;
+    const src = req.body.queryResult.parameters.source;
+    const vh_type = req.body.queryResult.parameters.vehicletype;
+    const bd_type = req.body.queryResult.parameters.bodytype; 
+    const pay_type = req.body.queryResult.parameters.payloadcapacity; 
+    const date = req.body.queryResult.parameters.searchdate; 
+
+    var options = {method: 'POST',
+        url: 'http://52.221.70.49:5032/api/v1/lane_search',
+        qs: 
+        { 
+         source: src,
+         destination:dst ,
+         vehicletype: vh_type,
+         bodytype: bd_type,
+         payloadcapacity: pay_type,
+         searchdate: date 
+        }
+    };
+    http.get(options, (responseFromAPI) => {
+        let completeResponse = '';
+        responseFromAPI.on('data', (chunk) => {
+            completeResponse += chunk;
+        });
+        responseFromAPI.on('end', () => {
+            const trux = JSON.parse(completeResponse);
+            dataToSend += `From ${src}  to ${dst} of vehicle type ${vh_type} the rate of turx is ${trux.values.trux_assured_rate} and the average rate is ${trux.values.average_rate}`;
+
+            return res.json({
+                speech: dataToSend,
+                displayText: dataToSend,
+                source: 'get-details'
+            });
+        });
+    }, (error) => {
+        return res.json({
+            speech: 'Something went wrong!',
+            displayText: 'Something went wrong!',
+            source: 'get-details'
+        });
+    });
+});
+
+server.listen((process.env.PORT || 80), () => {
+    console.log("Server is up and running...");
+});
