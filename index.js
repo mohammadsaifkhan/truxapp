823c772a94f54961de005be391197b8a3b914de4
mohammadsaifkhan <44085821+mohammadsaifkhan@users.noreply.github.com>
2019-01-29 10:07:20 +0530

    Update index.js

--- index.js
+++ index.js
@@ -16,47 +16,47 @@ app.use(bodyParser.json());
 
 app.get('/', (req, res) => {
 res.json("hello");
-//     const dst = req.body.queryResult.parameters.destination;
-//     const src = req.body.queryResult.parameters.source;
-//     const vh_type = req.body.queryResult.parameters.vehicletype;
-//     const bd_type = req.body.queryResult.parameters.bodytype; 
-//     const pay_type = req.body.queryResult.parameters.payloadcapacity; 
-//     const date = req.body.queryResult.parameters.searchdate; 
+    const dst = req.body.queryResult.parameters.destination;
+    const src = req.body.queryResult.parameters.source;
+    const vh_type = req.body.queryResult.parameters.vehicletype;
+    const bd_type = req.body.queryResult.parameters.bodytype; 
+    const pay_type = req.body.queryResult.parameters.payloadcapacity; 
+    const date = req.body.queryResult.parameters.searchdate; 
 
-//     var options = {method: 'POST',
-//         url: 'http://52.221.70.49:5032/api/v1/lane_search',
-//         qs: 
-//         { 
-//          source: src,
-//          destination:dst ,
-//          vehicletype: vh_type,
-//          bodytype: bd_type,
-//          payloadcapacity: pay_type,
-//          searchdate: date 
-//         }
-//     };
-//     http.post(options, (responseFromAPI) => {
-//         let completeResponse = '';
-//         responseFromAPI.on('data', (chunk) => {
-//             completeResponse += chunk;
-//         });
-//         responseFromAPI.on('end', () => {
-//             const trux = JSON.parse(completeResponse);
-//             dataToSend += `From ${src}  to ${dst} of vehicle type ${vh_type} the rate of turx is ${trux.values.trux_assured_rate} and the average rate is ${trux.values.average_rate}`;
+    var options = {method: 'POST',
+        url: 'http://52.221.70.49:5032/api/v1/lane_search',
+        qs: 
+        { 
+         source: src,
+         destination:dst ,
+         vehicletype: vh_type,
+         bodytype: bd_type,
+         payloadcapacity: pay_type,
+         searchdate: date 
+        }
+    };
+    http.get(options, (responseFromAPI) => {
+        let completeResponse = '';
+        responseFromAPI.on('data', (chunk) => {
+            completeResponse += chunk;
+        });
+        responseFromAPI.on('end', () => {
+            const trux = JSON.parse(completeResponse);
+            dataToSend += `From ${src}  to ${dst} of vehicle type ${vh_type} the rate of turx is ${trux.values.trux_assured_rate} and the average rate is ${trux.values.average_rate}`;
 
-//             return res.json({
-//                 speech: dataToSend,
-//                 displayText: dataToSend,
-//                 source: 'get-details'
-//             });
-//         });
-//     }, (error) => {
-//         return res.json({
-//             speech: 'Something went wrong!',
-//             displayText: 'Something went wrong!',
-//             source: 'get-details'
-//         });
-//     });
+            return res.json({
+                speech: dataToSend,
+                displayText: dataToSend,
+                source: 'get-details'
+            });
+        });
+    }, (error) => {
+        return res.json({
+            speech: 'Something went wrong!',
+            displayText: 'Something went wrong!',
+            source: 'get-details'
+        });
+    });
  });
 
 http.listen(8080), () => {
